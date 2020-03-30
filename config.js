64a721b7b1476bf63d662de37b6a9177df1ba714
tataicapo <tataicapo@gmail.com>
2020-03-29 23:04:10 -0300

    primera subida

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+    accountName: 'elmasmanco3',
+    password: 't1U2n3C@',
+    sharedSecret: 'Q+7/EUS34RU2WHUvAjkCWM9RunI=',
+    identitySecret: 'bqpc0kswz6PNNosbwP+7w29DqIc='
+}
---
+++ index.js
@@ -0,0 +1,98 @@
+const SteamUser = require('steam-user');
+const SteamTotp = require('steam-totp');
+const SteamCommunity = require('steamcommunity');
+const TradeOfferManager = require('steam-tradeoffer-manager');
+const config = require('./config');
+var express = require('express');
+var app = express();
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+
+app.get('/', function (req, res) {
+	res.send('Hello World!');
+});
+
+app.listen(server_port, function () {
+	console.log('Example app listening on port '+server_port);
+});
+
+const client = new SteamUser();
+const community = new SteamCommunity();
+const manager = new TradeOfferManager({
+	steam: client,
+	community: community,
+	language: 'es'
+});
+
+const logInOptions = {
+	accountName: config.accountName,
+	password: config.password,
+	twoFactorCode: SteamTotp.generateAuthCode(config.sharedSecret)
+};
+
+client.logOn(logInOptions);
+
+client.on('loggedOn', () => {
+	console.log('-logged on');
+	
+	client.setPersona(SteamUser.EPersonaState.Online);
+	client.gamesPlayed('nodejs tutorial');
+});
+
+client.on('webSession', (sid, cookies) => {
+	manager.setCookies(cookies);
+	community.setCookies(cookies);
+	community.startConfirmationChecker(20000, config.identitySecret);
+	sendFloralShirt();
+});
+
+manager.on('newOffer', offer => {
+	console.log('offer detected');
+	if (offer.partner.getSteamID64() === '76561198864031631') {
+		offer.accept((err, status) => {
+			if (err) {
+				console.log(err);
+			} else {
+				console.log(status);
+			}
+		})
+	} else {
+		console.log('unknown sender');
+		offer.decline(err => {
+			if (err) {
+				console.log(err);
+			} else {
+				console.log('trade from stranger declined');
+			}
+		});
+	}
+});
+
+function sendFloralShirt() {
+	console.log("-sendFloralShirt")
+	manager.loadInventory(730, 2, true, (err, inventory) => {
+		if (err) {
+			console.log('-loadInventory-error')
+			console.log(err);
+		} else {
+			console.log('-loadInventory-true')
+			const offer = manager.createOffer('76561198864031631');
+			//console.log(inventory)
+			inventory.forEach(function(item) {
+				if (item.assetid === '8507890831') {
+					offer.addMyItem(item);
+					offer.setMessage('You received a floral shirt!');
+					offer.send((err, status) => {
+						if (err) {
+							console.log(err);
+						} else {
+							console.log('trade sent');
+							console.log(status);
+						}
+					})
+				}
+			})
+		}
+	})
+}
